812aeb7f5157b98a858c3d4bb0adcbee123ccb65
Checkm8ra1n <superfrancybros@outlook.com>
2025-07-09 11:19:11 +0200

    Initial Commit

---
+++ Pina Colada/Pina_ColadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pina_ColadaApp.swift
+//  Pina Colada
+//
+//  Created by Checkm8Croft on 09/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Pina_ColadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
